b691a5c6e16ee1c128d1aa8ddb6f78bbd056f54a
we <we@localhost>
2022-09-09 14:37:33 +0530

    bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,26 @@
+import java.util.Arrays;
 
 public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr = {2,5,3,2,6};
+		bubbleSort(arr);
 
 	}
+	static void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+        System.out.println(Arrays.toString(arr));
+    }
+	
 
 }
